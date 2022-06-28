c67e02fc1d1c1a083a4c98b8dbd22917e4601413
Jérôme Pagny <pagny.jerome@gmail.com>
2022-06-28 14:41:05 +0200

    Add WebSecurityConfiguration + login controller + login.html

---
+++ src/main/java/com/paymybuddy/service/implement/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.paymybuddy.service.implement;
+
+import com.paymybuddy.entity.User;
+import com.paymybuddy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String addressEmail) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepository.findByEmailAddress(addressEmail);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with Email: " + addressEmail);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.get().getEmailAddress(), user.get().getPassword(), new ArrayList<>());
+    }
+
+}
