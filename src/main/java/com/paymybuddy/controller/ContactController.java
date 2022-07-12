4e643089f37c26d95efbf9049ec661d3d69055a9
Jérôme Pagny <pagny.jerome@gmail.com>
2022-07-12 17:14:00 +0200

    Add page contact

---
+++ src/main/java/com/paymybuddy/controller/ContactController.java
@@ -0,0 +1,15 @@
+package com.paymybuddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+}
